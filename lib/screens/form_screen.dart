22f86ac3381c77d1b81295042e5c9a964d8e1f3a
venicode <vitoria_aparecida2011@hotmail.com>
2023-12-21 10:49:40 -0300

    keyboards types and validator

--- lib/screens/form_screen.dart
+++ lib/screens/form_screen.dart
@@ -9,42 +9,130 @@ class FormScreen extends StatefulWidget {
 
 class _FormScreenState extends State<FormScreen> {
   TextEditingController nameController = TextEditingController();
+  TextEditingController difficultyController = TextEditingController();
+  TextEditingController imageController = TextEditingController();
+
+  final _formkey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            'Nova tarefa',
-            style: TextStyle(color: Colors.white),
+    return Form(
+      key: _formkey,
+      child: Scaffold(
+          appBar: AppBar(
+            title: const Text(
+              'Nova tarefa',
+              style: TextStyle(color: Colors.white),
+            ),
           ),
-        ),
-        body: Center(
-          child: Container(
-              height: 650,
-              width: 375,
-              decoration: BoxDecoration(
-                  color: Colors.black12,
-                  borderRadius: BorderRadius.circular(10),
-                  border: Border.all(width: 1)),
-              child: Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: TextFormField(
-                      controller: nameController,
-                      textAlign: TextAlign.center,
-                      decoration: const InputDecoration(
-                          border: OutlineInputBorder(),
-                          hintText: 'Digite o nome da tarefa',
-                          fillColor: Colors.white70,
-                          filled: true),
-                    ),
-                  ),
-                  ElevatedButton(onPressed: () {
-                    print(nameController.text);
-                  }, child: const Text('Adicionar!', style:  TextStyle(color: Colors.white),))
-                ],
-              )),
-        ));
+          body: Center(
+            child: SingleChildScrollView(
+              child: Container(
+                  height: 650,
+                  width: 375,
+                  decoration: BoxDecoration(
+                      color: Colors.black12,
+                      borderRadius: BorderRadius.circular(10),
+                      border: Border.all(width: 1)),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: TextFormField(
+                          validator: (String? value) {
+                            if (value != null && value.isEmpty) {
+                              return 'Insira o nome da tarefa';
+                            }
+                            return null;
+                          },
+                          controller: nameController,
+                          textAlign: TextAlign.center,
+                          decoration: const InputDecoration(
+                              border: OutlineInputBorder(),
+                              hintText: 'Digite o nome da tarefa',
+                              fillColor: Colors.white70,
+                              filled: true),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: TextFormField(
+                          validator: (value) {
+                            if (value!.isEmpty ||
+                                int.parse(value) > 5 ||
+                                int.parse(value) < 1) {
+                              return 'Insira um número válido de 1 a 5';
+                            }
+                            return null;
+                          },
+                          keyboardType: TextInputType.number,
+                          controller: difficultyController,
+                          textAlign: TextAlign.center,
+                          decoration: const InputDecoration(
+                              border: OutlineInputBorder(),
+                              hintText: 'Informe a dificuldade',
+                              fillColor: Colors.white70,
+                              filled: true),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: TextFormField(
+                          onChanged: (text) {
+                            setState(() {});
+                          },
+                          validator: (String? value){
+                            if (value!.isEmpty){
+                              return 'Insira o url da imagem';
+                            }
+                            return null;
+                          },
+                          keyboardType: TextInputType.url,
+                          controller: imageController,
+                          textAlign: TextAlign.center,
+                          decoration: const InputDecoration(
+                              border: OutlineInputBorder(),
+                              hintText: 'Imagem',
+                              fillColor: Colors.white70,
+                              filled: true),
+                        ),
+                      ),
+                      Container(
+                        height: 100,
+                        width: 100,
+                        decoration: BoxDecoration(
+                            color: Colors.green,
+                            borderRadius: BorderRadius.circular(10),
+                            border:
+                                Border.all(width: 2, color: Colors.lightGreen)),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(10),
+                          child: Image.network(
+                            imageController.text,
+                            errorBuilder: (BuildContext context, Object exception,
+                                StackTrace? stackTrace) {
+                              return Image.asset('assets/images/no-photos.png');
+                            },
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                      ElevatedButton(
+                          onPressed: () {
+                            if(_formkey.currentState!.validate()){
+                            print(nameController.text);
+                            print(difficultyController.text);
+                            print(imageController.text);
+                          }},
+                          child: const Text(
+                            'Adicionar!',
+                            style: TextStyle(color: Colors.white),
+                          ))
+                    ],
+                  )),
+            ),
+          )),
+    );
   }
 }
